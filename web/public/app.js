84a47b74f15629cca98abd294eae8ff831be6a71
Samarth Mistry <103932039+GeekySamOP@users.noreply.github.com>
2023-02-23 18:24:03 +0530

    Add files via upload

---
+++ web/public/app.js
@@ -0,0 +1,42 @@
+$('#navbar').load('navbar.html');
+
+const API_URL = 'http://localhost:5000/api';
+
+
+//const devices = JSON.parse(localStorage.getItem('devices')) || [];
+
+$.get(`${API_URL}/devices`)
+.then(response => {
+  response.forEach(device => {
+    $('#devices tbody').append(`
+      <tr>
+        <td>${device.user}</td>
+        <td>${device.name}</td>
+      </tr>`
+    );
+  });
+})
+
+.catch(error => {
+  console.error(`Error: ${error}`);
+});
+
+$('#add-device').on('click', () => {
+  const name = $('#name').val();
+  const user = $('#user').val();
+  const sensorData = [];
+
+  const body = {
+    name,
+    user,
+    sensorData
+  };
+
+  $.post(`${API_URL}/devices`, body)
+  .then(response => {
+    location.href = '/';
+  })
+  .catch(error => {
+    console.error(`Error: ${error}`);
+  });
+});
